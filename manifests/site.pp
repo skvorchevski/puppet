67761531391af380945c83e66a8817b4e9ae609a
Andrey Skvorchevskiy <askvorchevski@gmail.com>
2023-07-18 21:17:14 +0300

    production fix script

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,19 +8,23 @@ node slave1.puppet {
 		source => "/var/www/html/index.html",
   }
 
+	exec { 'open-port-80':
+		command => '/usr/bin/firewall-cmd --add-port=80/tcp --permanent',
+		path    => '/usr/bin',
+	}
+
 	service { 'httpd':
 		ensure => running,
 		enable => true,
-	}
-
-	exec { 'open-port-8080':
-		command => '/usr/bin/firewall-cmd --add-port=8080/tcp --permanent',
-		path    => '/usr/bin',
 	}
 }
 
 node slave2.puppet {
 	package { 'httpd':
+		ensure => installed,
+	}
+
+	package { 'php':
 		ensure => installed,
 	}
 
@@ -34,13 +38,21 @@ node slave2.puppet {
 		source => "/var/www/html/index.php",
 	}
 
+	exec { 'open-port-80':
+		command => '/usr/bin/firewall-cmd --add-port=80/tcp --permanent',
+		path    => '/usr/bin',
+	}
+
+	file { '/etc/httpd/conf/httpd.conf':
+		ensure => present,
+		content => "Listen 80\nServerName localhost\nDocumentRoot /var/www/html\nDirectoryIndex index.php\n",
+		require => Package['httpd'],
+		notify => Service['httpd'],
+	}
+
 	service { 'httpd':
 		ensure => running,
 		enable => true,
-	}
-
-	exec { 'open-port-8080':
-		command => '/usr/bin/firewall-cmd --add-port=8080/tcp --permanent',
-		path    => '/usr/bin',
+		require => File['/etc/httpd/conf/httpd.conf'],
 	}
 }
