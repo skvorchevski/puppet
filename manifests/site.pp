a36ca6b6faa748e53f720243f9e154343f06ae03
Andrey Skvorchevskiy <askvorchevski@gmail.com>
2023-07-18 18:48:59 +0300

    production added puppet manifest

---
+++ manifests/site.pp
@@ -0,0 +1,50 @@
+node slave1 {
+	package { 'httpd':
+		ensure => installed,
+	}
+
+	file { '/tmp/index.html':
+		ensure => present,
+		source => "/var/www/html/",
+    }
+
+	service { 'httpd':
+		ensure => running,
+		enable => true,
+	}
+
+	firewalld_port { 'Open port 8080 in the public zone':
+		ensure   => present,
+		zone     => public,
+		port     => 8080,
+		protocol => 'tcp',
+	}
+}
+
+node slave2 {
+	package { 'httpd':
+		ensure => installed,
+	}
+
+	file { 'index.html':
+		path => '/var/www/html/index.html',
+		ensure => absent,
+	}
+
+	file { '/tmp/index.php':
+		ensure => present,
+		source => "/var/www/html/",
+	}
+
+	service { 'httpd':
+		ensure => running,
+		enable => true,
+	}
+
+	firewalld_port { 'Open port 8080 in the public zone':
+		ensure   => present,
+		zone     => public,
+		port     => 8080,
+		protocol => 'tcp',
+	}
+}
