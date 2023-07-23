415e270a6693c052eaa2c655b7ae613ff1c3bc98
Andrey Skvorchevskiy <askvorchevski@gmail.com>
2023-07-23 14:34:10 +0300

    production fix profile_nginx

--- site/profile/manifests/profile_nginx.pp
+++ site/profile/manifests/profile_nginx.pp
@@ -1,5 +1,5 @@
 class profile::profile_nginx (
-  String $conf_source = 'puppet:///files/nginx-conf',
+  String $conf_source = 'puppet:///files/nginx.conf',
 ) {
   class { 'nginx':
     nginx_conf_source => $conf_source
