b64d5fee74693cef1cd708bdf5511d70cff30f6f
Andrey Skvorchevskiy <askvorchevski@gmail.com>
2023-07-23 15:14:33 +0300

    production fix init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
     before => File['minecraft_file']
   }
 
-  file { 'minecraft_file':
+  file { 'minecraft_server.1.20.1.jar':
     path    => "/opt/minecraft",
     source  => "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar",
     mode    => "755",
