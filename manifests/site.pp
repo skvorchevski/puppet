fd6bbd7162bf37d756afb240bf70fdb1d6c8d9bc
Andrey Skvorchevskiy <askvorchevski@gmail.com>
2023-07-23 13:59:38 +0300

    production fix site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,15 @@
 node slave1.puppet {
-	include role::slave1_machine
+  include role::slave1_machine
 }
 
 node slave2.puppet {
-	include role::slave2_machine
+  include role::slave2_machine
+}
+
+node mineserver.puppet {
+  include minecraft
 }
 
 node master.puppet {
-	include role::master_machine
-  include minecraft
+  include role::master_machine
 }
